1c5cb89e74aca8dcf5d3a96ee5238a7ffa141fab
Alexey Obryadin <obryadin.alexey@gmail.com>
2023-12-27 21:01:36 +0300

    config

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -3,17 +3,12 @@ import type { Configuration } from 'webpack';
 import { rules } from './webpack.rules';
 import { plugins } from './webpack.plugins';
 
-rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-});
-
 export const rendererConfig: Configuration = {
-  module: {
-    rules,
-  },
-  plugins,
-  resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
-  },
+	module: {
+		rules,
+	},
+	plugins,
+	resolve: {
+		extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+	},
 };
