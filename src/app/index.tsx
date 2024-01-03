8f1f2a832ad2196cf120712858c61ab3f6dc6691
Alexey Obryadin <obryadin.alexey@gmail.com>
2024-01-03 21:34:21 +0300

    add styles

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Main from '../pages/main';
+import { BrowserRouter } from 'react-router-dom';
+import Router from '../pages';
+import './styles/index.scss';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<Main />
+		<BrowserRouter basename="/main_window">
+			<Router />
+		</BrowserRouter>
 	</React.StrictMode>
 );
