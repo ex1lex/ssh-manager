795aa5bae208097e01cc1d704f673b85fc9af1d4
Alexey Obryadin <obryadin.alexey@gmail.com>
2024-01-04 01:32:49 +0300

    add toasts

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Router from '../pages';
 import './styles/index.scss';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
+
 root.render(
 	<React.StrictMode>
 		<BrowserRouter basename="/main_window">
 			<Router />
 		</BrowserRouter>
+		<ToastContainer />
 	</React.StrictMode>
 );
