3cca933c84a19866484de1c94217800e1bdb671b
Alexey Obryadin <obryadin.alexey@gmail.com>
2024-01-03 21:30:21 +0300

    svg icon component

---
+++ src/widgets/svg-icon/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export enum Icons {
+	delete = 'delete',
+	edit = 'edit',
+}
+
+interface Props {
+	icon: Icons;
+}
+
+const SvgIcon = ({ icon }: Props) => {
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
+	const Svg = require(`./icons/${icon}`);
+	return <Svg.default />;
+};
+
+export default SvgIcon;
