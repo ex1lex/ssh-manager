3215745106e79449c2cab0db02080af7a019877a
Alexey Obryadin <obryadin.alexey@gmail.com>
2024-01-09 15:40:26 +0300

    add btn

--- src/widgets/actions/index.tsx
+++ src/widgets/actions/index.tsx
@@ -1,5 +1,7 @@
+import classNames from 'classnames';
 import React, { FC } from 'react';
 
+import SvgIcon, { Icons } from '../svg-icon';
 import styles from './styles.module.scss';
 
 const Actions: FC = () => {
@@ -7,6 +9,15 @@ const Actions: FC = () => {
 		<div className={styles.actions}>
 			<button className={styles.actions__btn} type="button">
 				Create config
+			</button>
+			<button
+				className={classNames(
+					styles.actions__btn,
+					styles.actions__btn_type_svg
+				)}
+				type="button"
+			>
+				<SvgIcon icon={Icons.refresh} />
 			</button>
 		</div>
 	);
