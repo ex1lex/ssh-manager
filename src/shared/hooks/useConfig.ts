c1b7ccf6e73769d33131aa903da99ed954349e04
Alexey Obryadin <obryadin.alexey@gmail.com>
2024-01-09 16:26:32 +0300

    modify hook

--- src/shared/hooks/useConfig.ts
+++ src/shared/hooks/useConfig.ts
@@ -2,15 +2,20 @@ import { useEffect } from 'react';
 
 import { useAppDispatch } from '../../app/redux';
 import { getConfigSelector, setConfigs } from '../../app/redux/slices/configs';
+import { TConfig } from '../types';
 
 export const useConfig = () => {
 	const dispatch = useAppDispatch();
 
-	const configs = getConfigSelector();
+	const configs: TConfig[] = getConfigSelector();
 
-	const getConfigs = async () => {
+	const _getConfigs = async () => {
 		const newConfig: any = await window.electron.getListOfConfigs();
 		dispatch(setConfigs(newConfig));
+	};
+
+	const getConfig = (val: string): TConfig | undefined => {
+		return configs.find((item: any) => item?.value?.toString() === val);
 	};
 
 	const onDelete = async (val: string) => {
@@ -19,12 +24,14 @@ export const useConfig = () => {
 	};
 
 	const onRefresh = async () => {
-		await getConfigs();
+		await _getConfigs();
 	};
 
 	useEffect(() => {
-		getConfigs();
+		if (!configs.length) {
+			_getConfigs();
+		}
 	}, []);
 
-	return { configs, onDelete, onRefresh };
+	return { configs, onDelete, onRefresh, getConfig };
 };
